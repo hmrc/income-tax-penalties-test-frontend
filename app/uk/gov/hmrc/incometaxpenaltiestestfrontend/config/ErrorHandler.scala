3d3a0281c79a3dc82104229653cbc660e347fa0e
muhammadaliahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2025-04-16 16:55:00 +0100

    agent controller added

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/config/ErrorHandler.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,26 @@
 
 package uk.gov.hmrc.incometaxpenaltiestestfrontend.config
 
-import javax.inject.{Inject, Singleton}
-
 import play.api.i18n.MessagesApi
-import play.api.mvc.RequestHeader
+import play.api.mvc.{RequestHeader, Result}
+import play.api.mvc.Results.InternalServerError
 import play.twirl.api.Html
-import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.views.html.ErrorTemplate
+import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ErrorHandler @Inject()(
   errorTemplate: ErrorTemplate,
-  override val messagesApi: MessagesApi
+  override val messagesApi: MessagesApi,
 )(implicit override val ec: ExecutionContext
 ) extends FrontendErrorHandler {
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: RequestHeader): Future[Html] =
     Future.successful(errorTemplate(pageTitle, heading, message))
+
+  def showInternalServerError()(implicit request: RequestHeader): Future[Result] =
+    internalServerErrorTemplate.map(InternalServerError(_))
 }
--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/connectors/SessionDataConnector.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/connectors/SessionDataConnector.scala
@@ -29,32 +29,32 @@
 // * See the License for the specific language governing permissions and
 // * limitations under the License.
 // */
-//
-//package uk.gov.hmrc.incometaxpenaltiestestfrontend.connectors
-//
-//import play.api.libs.json.Json
-//import uk.gov.hmrc.http.client.HttpClientV2
-//import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
-//import uk.gov.hmrc.incometaxpenaltiestestfrontend.config.AppConfig
-//import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.SessionDataModel
-//import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.SessionDataPostResponse.SessionDataPostResponse
-//
-//import javax.inject.Inject
-//import scala.concurrent.{ExecutionContext, Future}
-//
-//class SessionDataConnector @Inject()(val appConfig: AppConfig,
-//                                     val http: HttpClientV2
-//                                    )(implicit ec: ExecutionContext) extends RawResponseReads {
-//
-//  def postSessionData(sessionDataModel: SessionDataModel)(implicit hc: HeaderCarrier): Future[SessionDataPostResponse] = {
-//    lazy val url = s"${appConfig.incomeTaxSessionDataUrl}/income-tax-session-data/"
-//
-//    val body = Json.toJson[SessionDataModel](sessionDataModel)
-//
-//    http
-//      .post(url"$url")
-//      .withBody(body)
-//      .execute[SessionDataPostResponse]
-//
-//  }
-//}
+
+package uk.gov.hmrc.incometaxpenaltiestestfrontend.connectors
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.config.AppConfig
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.SessionDataModel
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.SessionDataPostResponse.SessionDataPostResponse
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class SessionDataConnector @Inject()(val appConfig: AppConfig,
+                                     val http: HttpClientV2
+                                    )(implicit ec: ExecutionContext) extends RawResponseReads {
+
+  def postSessionData(sessionDataModel: SessionDataModel)(implicit hc: HeaderCarrier): Future[SessionDataPostResponse] = {
+    lazy val url = s"${appConfig.incomeTaxSessionDataUrl}/income-tax-session-data/"
+
+    val body = Json.toJson[SessionDataModel](sessionDataModel)
+
+    http
+      .post(url"$url")
+      .withBody(body)
+      .execute[SessionDataPostResponse]
+
+  }
+}
