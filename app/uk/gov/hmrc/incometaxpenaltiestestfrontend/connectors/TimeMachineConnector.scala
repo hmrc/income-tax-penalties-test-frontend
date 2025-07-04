409e61978c336e91c81bb5c913fc830fe747e70b
nadinJ <63252254+nadinJ@users.noreply.github.com>
2025-07-04 11:48:40 +0100

    MIPR-1882: added TimeMachine

---
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/connectors/TimeMachineConnector.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxpenaltiestestfrontend.connectors
+
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.config.AppConfig
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class TimeMachineConnector @Inject()(val appConfig: AppConfig,
+                                     val http: HttpClientV2
+                                    )(implicit ec: ExecutionContext) extends RawResponseReads {
+
+  def updatePenalties(timeMachineDate: String)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    lazy val url = s"${appConfig.incomeTaxPenaltiesUrl}/view-penalty/self-assessment/test-only/time-machine-now?dateToSet=$timeMachineDate"
+
+    http
+      .get(url"$url")
+      .execute[HttpResponse]
+
+  }
+
+  def resetPenalties()(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    lazy val url = s"${appConfig.incomeTaxPenaltiesUrl}/view-penalty/self-assessment/test-only/time-machine-now"
+    http
+      .get(url"$url")
+      .execute[HttpResponse]
+  }
+
+  def updatePenaltiesAppeals(timeMachineDate: String)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    lazy val url = s"${appConfig.incomeTaxPenaltiesAppealsUrl}/view-penalty/self-assessment/test-only/time-machine-now?dateToSet=$timeMachineDate"
+
+    http
+      .get(url"$url")
+      .execute[HttpResponse]
+
+  }
+
+  def resetPenaltiesAppeals()(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    lazy val url = s"${appConfig.incomeTaxPenaltiesAppealsUrl}/view-penalty/self-assessment/test-only/time-machine-now"
+    http
+      .get(url"$url")
+      .execute[HttpResponse]
+  }
+}
