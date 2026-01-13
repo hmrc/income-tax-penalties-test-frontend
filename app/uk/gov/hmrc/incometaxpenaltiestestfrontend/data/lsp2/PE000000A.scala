fc891859e305aae58a37e26088a5b777c300d386
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-01-13 15:52:11 +0000

    MIPR-2276-b: changes for ui test

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp2/PE000000A.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp2/PE000000A.scala
@@ -32,6 +32,8 @@ object PE000000A extends UserDetailsData {
   val lspPenalty2 = LateSubmissionPenaltyDetails.active(
     ReportingPeriod(2027, Some(2)),
     penaltyOrder = "2"
+  ).copy(
+    communicationsDate = Some("2023-11-07"),
   )
 
   override val lsp: Option[LSP] = Some(LSP(
