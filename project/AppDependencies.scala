8ea705b811bfaafd64accac9c8cacdce2f759487
kSiadat <49532979+kSiadat@users.noreply.github.com>
2026-02-12 14:32:38 +0000

    MIPR-2396: dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.5.0"
   
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.27.0"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.31.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion    % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.20.1"            % Test,
+    "org.jsoup"               %  "jsoup"                      % "1.22.1"            % Test,
   )
 
   val it: Seq[Nothing] = Seq.empty
