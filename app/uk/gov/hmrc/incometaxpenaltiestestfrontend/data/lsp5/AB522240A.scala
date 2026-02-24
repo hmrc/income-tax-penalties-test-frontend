e6b54c75468db6e01e5d4a41900a2e82bbb45956
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-02-24 15:32:51 +0000

    add new nino

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp5/AB522240A.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp5/AB522240A.scala
@@ -19,9 +19,9 @@ package uk.gov.hmrc.incometaxpenaltiestestfrontend.data.lsp5
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.data.{LateSubmissionPenaltyDetails, UserDetailsData}
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.ReportingPeriod
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.complianceData.CompliancePayload
-import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{AppealInformation, LSP, LSPSummary}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{AppealInformation, BreathingSpace, LSP, LSPSummary}
 
-object AB511140A extends UserDetailsData {
+object AB522240A extends UserDetailsData {
 
   val lspSummary = LSPSummary(
     activePenaltyPoints = 5,
@@ -44,12 +44,12 @@ object AB511140A extends UserDetailsData {
     )
 
   val lspPenalty3 = LateSubmissionPenaltyDetails.active(
-    ReportingPeriod(2027, Some(1)),
-    penaltyOrder = "3",
-    addAdditionalIncomeSource = true)
+      ReportingPeriod(2027, Some(1)),
+      penaltyOrder = "3",
+      addAdditionalIncomeSource = true)
     .withAppealInformation(
-    AppealInformation.create("Rejected", "Tribunal")
-  )
+      AppealInformation.create("Rejected", "Tribunal")
+    )
 
   val lspPenalty4 = LateSubmissionPenaltyDetails.active(
     ReportingPeriod(2027, Some(0)),
@@ -73,7 +73,12 @@ object AB511140A extends UserDetailsData {
       lspPenalty3, lspPenalty4, lspPenalty5,
       lspPenalty6)
   ))
-
+  override val breathingSpace: Option[Seq[BreathingSpace]] = Some(Seq(
+    BreathingSpace(
+      bsStartDate = "2027-08-30",
+      bsEndDate = "2028-08-30"
+    )
+  ))
   override def optComplianceData: Option[CompliancePayload] = Some(
     CompliancePayload.apply(nino)
       .withObligationDetail(ReportingPeriod(2028, Some(2)), false)
@@ -87,11 +92,10 @@ object AB511140A extends UserDetailsData {
       .withObligationDetail(ReportingPeriod(2026, Some(3)), false)
       .withObligationDetail(ReportingPeriod(2026, None), true)
   )
-  override val nino: String = "AB511140A"
-  override val description: String = "LSP5, 5 points latest tax return due, oldest penalty cancelled"
-  override val descriptionOverdue: Option[String] = Some("LSP5, 5 points latest tax return overdue, oldest penalty cancelled")
+  override val nino: String = "AB522240A"
+  override val description: String = "Breathing Space LSP5, 5 points latest tax return due, oldest penalty cancelled"
   override val timemachineDate: String = "03/03/2028"
-  override val timeMachineDateOverdue: Option[String] = Some("04/04/2028")
   override val mtdItId: String = "XTIT12345511140"
   override val utr: String = "0000511140"
 }
+
