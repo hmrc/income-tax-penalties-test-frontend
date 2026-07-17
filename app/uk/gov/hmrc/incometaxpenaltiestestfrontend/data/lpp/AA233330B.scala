16ef5730349a3a64cc9c1755dd6e62fba6bb569b
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-07-17 16:25:04 +0100

    MIPR-2822: Fixed LPP2 supplementary assessment nino

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lpp/AA233330B.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lpp/AA233330B.scala
@@ -19,35 +19,58 @@ package uk.gov.hmrc.incometaxpenaltiestestfrontend.data.lpp
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.data.{LatePaymentPenaltyDetails, UserDetailsData}
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.ReportingPeriod
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.financialData.FinancialData
-import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{LPP, Totalisations}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{LPP, LPPDetails, Totalisations}
 
 object AA233330B extends UserDetailsData {
 
   override val totalisations: Option[Totalisations] = Some(
     Totalisations(
-      lppEstimatedTotal = 46.00
+      lppEstimatedTotal = 320.81
     )
   )
 
-  lazy val latePaymentPenaltyDetails1 = LatePaymentPenaltyDetails.lpp2DueOrOverdue(
-    ReportingPeriod(2027, None),
-    46.02,
-    latePaymentPenaltyDetails2.principalChargeReference
+  private val principalChargeReference = "XJ002616061062"
+
+  // LPP2, no supplement, 19.17
+  val latePaymentPenaltyDetails1: LPPDetails = LatePaymentPenaltyDetails.lpp2DueOrOverdue(
+    ReportingPeriod(2025, None),
+    19.17,
+    principalChargeReference
+  ).copy(
+    lpp1LRCalculationAmt = Some(5000),
+    lpp1HRCalculationAmt = Some(5000),
+    penaltyChargeDueDate = Some("2026-04-17"),
+    principalChargeLatestClearing = Some("2026-03-16")
+  )
+
+  // LPP1, no supplement, 300
+  val latePaymentPenaltyDetails2: LPPDetails = LatePaymentPenaltyDetails.lpp1DueOrOverdue(
+    ReportingPeriod(2025, None),
+    amount = 300,
+    optChargeRef = Some(principalChargeReference)
+  ).copy(
+    lpp1LRCalculationAmt = Some(5000),
+    lpp1HRCalculationAmt = Some(5000),
+    principalChargeLatestClearing = Some("2026-03-16")
+  )
+
+  // LPP2, supplement=true, 1.64
+  val latePaymentPenaltyDetails3: LPPDetails = LatePaymentPenaltyDetails.lpp2DueOrOverdue(
+    ReportingPeriod(2025, None),
+    1.64,
+    principalChargeReference
+  ).copy(
+    lpp1LRCalculationAmt = Some(5000),
+    lpp1HRCalculationAmt = Some(5000),
+    penaltyChargeCreationDate = Some("2026-03-20"),
+    penaltyChargeDueDate = Some("2026-04-26"),
+    principalChargeLatestClearing = Some("2026-03-25")
   ).withSupplementary(supplement = Some(true))
-
-  val latePaymentPenaltyDetails2 = LatePaymentPenaltyDetails.lpp1Paid(
-    ReportingPeriod(2027, None),
-    amount = 120.00
-  )
-
-  val latePaymentPenaltyDetails3 = LatePaymentPenaltyDetails.lpp1Paid(
-    ReportingPeriod(2026, None),
-    amount = 60.00
-  )
 
   override def optFinancialData(): Option[FinancialData] = Some(
     FinancialData.create(
-      totalAccountPostedInterest = Some(46.02)
+      totalAccountOverdue = Some(0.00),
+      totalAccountPostedInterest = Some(320.81)
     )
   )
 
@@ -59,9 +82,9 @@ object AA233330B extends UserDetailsData {
   override val nino: String = "AA233330B"
   override val mtdItId: String = "23333"
   override val utr: String = "1000023333"
-  override val description: String = "3 LPPs - (1 LPP2 DUE, 2 LPP1s PAID) with supplementary charge"
-  override val descriptionOverdue: Option[String] = Some("3 LPPs - (1 LPP2 OVERDUE, 2 LPP1s PAID) with supplementary charge")
-  override val timemachineDate: String = "09/04/2028"
-  override val timeMachineDateOverdue: Option[String] = Some("20/05/2028")
+  override val description: String = "3 LPPs - (1 LPP2 DUE, 1 LPP1 DUE, 1 supplementary LPP2 DUE) with supplementary charge"
+  override val descriptionOverdue: Option[String] = Some("3 LPPs - (1 LPP2 OVERDUE, 1 LPP1 OVERDUE, 1 supplementary LPP2 OVERDUE) with supplementary charge")
+  override val timemachineDate: String = "26/03/2026"
+  override val timeMachineDateOverdue: Option[String] = Some("20/05/2026")
 
 }
