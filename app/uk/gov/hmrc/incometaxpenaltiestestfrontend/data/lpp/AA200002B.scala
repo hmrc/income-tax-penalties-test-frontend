cf4a07ed930ec071c6812981496fc9c43a836e33
Muhammad-Ali-Ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2026-08-25 09:48:25 +0100

    new nino added for breathing pace (#93)
    * new nino added for breathing pace
    
    * ninos updated
    
    * updated
    
    * userdata updated
    
    * descriptionupdate
    
    * date and desc updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated

---
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lpp/AA200002B.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxpenaltiestestfrontend.data.lpp
+
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.data.{LatePaymentPenaltyDetails, UserDetailsData}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.ReportingPeriod
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.financialData.FinancialData
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{BreathingSpace, LPP, Totalisations}
+
+object AA200002B extends UserDetailsData {
+
+  override val totalisations: Option[Totalisations] = Some(
+    Totalisations(
+      lppEstimatedTotal = 16.98,
+      lppPostedTotal = 120
+    )
+  )
+
+  lazy val latePaymentPenaltyDetails1 = LatePaymentPenaltyDetails.lpp2Penalty(
+    ReportingPeriod(2027, None),
+    16.98,
+    latePaymentPenaltyDetails2.principalChargeReference
+  ).copy(penaltyChargeDueDate = Some("2028-05-03"), penaltyChargeCreationDate = Some("2028-04-02"), lpp1HRCalculationAmt = Some(2000.00), lpp1LRCalculationAmt = Some(2000.00))
+
+  val latePaymentPenaltyDetails2 = LatePaymentPenaltyDetails.lpp1DueOrOverdue(
+    ReportingPeriod(2027, None),
+    amount = 120.00
+  ).copy(penaltyChargeDueDate = Some("2028-05-03"), penaltyChargeCreationDate = Some("2028-04-02"))
+  
+
+  override def optFinancialData(): Option[FinancialData] = Some(
+    FinancialData.create(
+      totalAccountAccruingInterest = Some(16.98),
+      totalAccountPostedInterest = Some(120.00)
+    )
+  )
+
+  override val breathingSpace: Option[Seq[BreathingSpace]] = Some(Seq(
+    BreathingSpace(
+      bsStartDate = "2028-02-01",
+      bsEndDate = "2028-04-01"
+    )
+  ))
+
+  override val lpp: Option[LPP] = Some(LPP(
+    manualLPPIndicator = false,
+    lppDetails = Some(Seq(latePaymentPenaltyDetails1, latePaymentPenaltyDetails2))
+  ))
+
+  override val nino: String = "AA200002B"
+  override val mtdItId: String = "20000"
+  override val utr: String = "1000020000"
+  override val description: String = "Breathing Space (Charge after BS) - 2 LPPs (1 LPP2 BS ESTIMATE, 1 LPP1 BS DUE)"
+  override val timemachineDate: String = "02/05/2028"
+}
