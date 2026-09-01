d03284f135f5f548a69a2b5b35fc2aa2ee29ab70
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-09-01 11:07:59 +0100

    new nino added

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lpp/AA200003A.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lpp/AA200003A.scala
@@ -25,34 +25,37 @@ object AA200003A extends UserDetailsData {
 
   override val totalisations: Option[Totalisations] = Some(
     Totalisations(
-      lppEstimatedTotal = 9.86,
-      lppPostedTotal = 120
+      lppEstimatedTotal = 0,
+      lppPostedTotal = 120,
+      totalAccountOverdue = 10.00
     )
   )
 
-  lazy val latePaymentPenaltyDetails1 = LatePaymentPenaltyDetails.lpp2DueOrOverdue(
+  lazy val latePaymentPenaltyDetails1 = LatePaymentPenaltyDetails.lpp2PartiallyPaid(
     ReportingPeriod(2027, None),
-    9.86,
+    13.15,
+    5.00,
     latePaymentPenaltyDetails2.principalChargeReference
-  ).withIncomeTaxPaid(ReportingPeriod(2027, None), false).copy(lpp1HRCalculationAmt = Some(2000.00), lpp1LRCalculationAmt = Some(2000.00), penaltyChargeDueDate = Some("2028-04-26"), principalChargeLatestClearing = Some("2028-03-25"))
+  ).copy(lpp1HRCalculationAmt = Some(2000.00), lpp1LRCalculationAmt = Some(2000.00), penaltyChargeDueDate = Some("2028-06-26"), principalChargeLatestClearing = Some("2028-05-25"))
+
 
   val latePaymentPenaltyDetails2 = LatePaymentPenaltyDetails.lpp1DueOrOverdue(
     ReportingPeriod(2027, None),
     amount = 120.00
-  ).copy(penaltyChargeDueDate = Some("2028-04-03"), principalChargeLatestClearing = Some("2028-03-25"))
-  
+  )
+
 
   override def optFinancialData(): Option[FinancialData] = Some(
     FinancialData.create(
-      totalAccountAccruingInterest = Some(9.86),
+      totalAccountAccruingInterest = Some(13.15),
       totalAccountPostedInterest = Some(120.00)
     )
   )
 
   override val breathingSpace: Option[Seq[BreathingSpace]] = Some(Seq(
     BreathingSpace(
-      bsStartDate = "2028-03-20",
-      bsEndDate = "2028-05-19"
+      bsStartDate = "2028-03-15",
+      bsEndDate = "2028-05-14"
     )
   ))
 
@@ -64,6 +67,6 @@ object AA200003A extends UserDetailsData {
   override val nino: String = "AA200003A"
   override val mtdItId: String = "20000"
   override val utr: String = "1000020000"
-  override val description: String = "Breathing Space (Charge before BS) - 2 LPPs (1 LPP2 OVERDUE, 1 LPP1 OVERDUE"
-  override val timemachineDate: String = "1/07/2028"
+  override val description: String = "Breathing Space- (Charge before and after BS) - 2 LPPs (1 LPP2 OVERDUE Partially paid, 1 LPP1 OVERDUE)"
+  override val timemachineDate: String = "01/07/2028"
 }
