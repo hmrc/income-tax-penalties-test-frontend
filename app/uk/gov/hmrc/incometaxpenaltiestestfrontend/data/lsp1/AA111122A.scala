48f75363e44a6029cf104feead509fe02b6477de
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-09-07 15:08:44 +0100

    Update AA111122A with static penaltyNumber

--- app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp1/AA111122A.scala
+++ app/uk/gov/hmrc/incometaxpenaltiestestfrontend/data/lsp1/AA111122A.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.incometaxpenaltiestestfrontend.data.lsp1
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.data.{LateSubmissionPenaltyDetails, UserDetailsData}
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.ReportingPeriod
 import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.complianceData.CompliancePayload
-import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{AppealInformation, LSP, LSPSummary}
+import uk.gov.hmrc.incometaxpenaltiestestfrontend.models.hip.penaltyDetails.{AppealInformation, LSP, LSPSummary, LSPDetails}
 
 object AA111122A extends UserDetailsData {
 
@@ -28,12 +28,12 @@ object AA111122A extends UserDetailsData {
   )
 
   val appealInformation = AppealInformation.create("UnderAppeal", "Tribunal")
-
-  val lspPenalty1 = LateSubmissionPenaltyDetails.active(
-    ReportingPeriod(2027, Some(1)),
-    returnSubmitted = true,
-    addAdditionalIncomeSource = true)
-    .withAppealInformation(appealInformation)
+  private val penaltyNumber = "005000001073"
+  val lspPenalty1: LSPDetails = LateSubmissionPenaltyDetails.active(
+      ReportingPeriod(2027, Some(1)),
+      returnSubmitted = true,
+      addAdditionalIncomeSource = true)
+    .withAppealInformation(appealInformation).withPenaltyNumber(penaltyNumber)
 
   override val lsp: Option[LSP] = Some(LSP(
     lspSummary = lspSummary,
@@ -41,6 +41,7 @@ object AA111122A extends UserDetailsData {
   ))
 
   override def optComplianceData: Option[CompliancePayload] = None
+
   override val nino: String = "AA111122A"
   override val mtdItId: String = "11122"
   override val utr: String = "1000011122"
